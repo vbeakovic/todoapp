a1d19db1dee554e61f271008aa1d9679e342a122
vbeakovic <valter.beakovic@gmail.com>
2018-04-10 20:09:23 +0200

    Read todos from collection

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+
 const path = require('path');
 const bodyParser = require('body-parser');
 
 // Port
 const port = 3000;
+// Mongo path
+const url = 'mongodb://localhost:27017/todoapp';
 
 // Initialize app
 const app = express();
@@ -17,10 +21,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res, next) => {
-  res.render('index');
-});
+// Connect to mongodb
+MongoClient.connect(url, (err, database) => {
+  if (err) throw err;
+  console.log('MongoDB connected...');
 
-app.listen(port, () => {
-  console.log('Server running on port ' + port);
+  //db = database;
+  db = database.db('todoapp');
+  Todos = db.collection('todos');
+  app.listen(port, () => {
+    console.log('Server running on port ' + port);
+  });
+})
+
+app.get('/', (req, res, next) => {
+  Todos.find({}).toArray((err, todos) => {
+    if (err) {
+      return console.log(err);
+    }
+    console.log(todos);
+    res.render('index', {
+      todos: todos
+    });
+  });
 });
